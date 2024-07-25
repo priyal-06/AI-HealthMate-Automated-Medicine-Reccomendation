7726dfc58b0df31de7b206397ff28aafcba70486
priyal-06 <143326254+priyal-06@users.noreply.github.com>
2024-07-25 13:51:08 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,8 @@
+const GetStarted=document.querySelector('.btn');
+GetStarted.addEventListener('click',()=>{
+    window.location.href='logging.html';
+})
+const home=document.querySelector('.home');
+home.addEventListener('click',()=>{
+    window.location.href='home.html';
+})
